17171fb407c584225cc3e5026a53058d71240669
Politecnico Desarrollo de software <poli.software.2025@gmail.com>
2025-08-22 19:07:55 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div rol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -172,7 +172,7 @@ export default function HomePage() {
                   <Menu className="h-6 w-6" />
                 </Button>
               </SheetTrigger>
-              <SheetContent side="left" className="w-64 bg-primary p-6 text-primary-foreground">
+              <SheetContent side="left" className="w-64 bg-[#ABD0BC] p-6 text-primary-foreground">
                 <SheetTitle className="sr-only">Navegación Móvil</SheetTitle>
                 <nav className="flex flex-col items-start space-y-6 pt-8">
                   {navLinks.map((link) => (
