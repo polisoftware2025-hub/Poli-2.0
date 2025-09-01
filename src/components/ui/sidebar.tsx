806aa8bfdd14e954e66ecb9389e63c52222c4eaf
Sebastian Pabon <sebastian.pabon.poli@gmail.com>
2025-09-01 00:27:57 +0000

    Cuando se genera un scroll-x los botones de la navegación izquierda se s

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -553,6 +553,7 @@ const SidebarMenuButton = React.forwardRef<
       size = "default",
       tooltip,
       className,
+      children,
       ...props
     },
     ref
@@ -566,9 +567,11 @@ const SidebarMenuButton = React.forwardRef<
         data-sidebar="menu-button"
         data-size={size}
         data-active={isActive}
-        className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
+        className={cn(sidebarMenuButtonVariants({ variant, size }), "whitespace-nowrap truncate", className)}
         {...props}
-      />
+      >
+        {children}
+      </Comp>
     )
 
     if (!tooltip) {
