24d34840457fe16e1d09081824b657eb2116e52f
Politecnico Desarrollo de software <poli.software.2025@gmail.com>
2025-10-23 18:53:39 +0000

    arregla los formularios porque mira como estan

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,3 +1,4 @@
+
 import * as React from 'react';
 
 import {cn} from '@/lib/utils';
@@ -8,7 +9,6 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
       <textarea
         className={cn(
           'flex min-h-[80px] w-full rounded-md border border-input bg-background/50 px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-          'dark:bg-black/40 dark:border-cyan-300/30 dark:placeholder:text-gray-400 dark:text-white',
           className
         )}
         ref={ref}
