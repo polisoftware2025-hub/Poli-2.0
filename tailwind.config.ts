f133dc1df1b7e0a47a66e2ab7703163b6884cfb3
Politecnico Desarrollo de software <poli.software.2025@gmail.com>
2025-10-23 18:33:54 +0000

    dale un diseño diferente a todos los scrolls mas futurista

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,7 +79,9 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require('tailwind-scrollbar')],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
+
+    
